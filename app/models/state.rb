083eecf8016511ca6334f7b2a003e773a587e6fe
Tom Corley <tom.corley@goodmeasures.com>
2017-03-06 00:02:56 -0500

    initial commit

---
+++ app/models/state.rb
@@ -0,0 +1,19 @@
+class State < ApplicationRecord
+  CODE_TYPE = "ST"
+
+  DECODER = {"id" => "code"}
+
+  def self.import_states(states)
+    states.map do |state_hash|
+      updated_hash = state_hash.except("type").map{|key, value| [DECODER[key] || key, value]}.to_h
+      state = State.find_by_name(updated_hash["name"])
+      next if state
+      State.create(updated_hash)
+    end
+  end
+
+  has_many :counties, :inverse_of => :state, :dependent => :destroy
+  has_many :cities, :inverse_of => :state, :dependent => :destroy
+  has_many :points_of_interest, :inverse_of => :state, :dependent => :destroy
+  has_many :properties, :inverse_of => :state, :dependent => :destroy
+end
---
+++ db/migrate/20170306013452_create_states.rb
@@ -0,0 +1,14 @@
+class CreateStates < ActiveRecord::Migration[5.0]
+  def change
+    create_table :states do |t|
+      t.string :name
+      t.string :abbreviation
+      t.string :code
+      t.string :geo_key
+      t.float  :geo_center_latitude
+      t.float  :geo_center_longitude
+
+      t.timestamps
+    end
+  end
+end
